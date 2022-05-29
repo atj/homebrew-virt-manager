a774e15b73c145dda2906e9bfa23fc52d8a6be18
Adam James <atj@pulsewidth.org.uk>
2022-05-29 10:14:49 +0100

    Bump os-info-db-tools to 1.9.0

--- osinfo-db-tools.rb
+++ osinfo-db-tools.rb
@@ -1,27 +1,34 @@
 class OsinfoDbTools < Formula
   desc "Tools for managing the libosinfo database files"
   homepage "https://libosinfo.org"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.6.0.tar.gz"
-  sha256 "d0d5b1196d73a7abed051be48d0e5b4aa196aac4cdbf8ddf52f57c0c492b2574"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.9.0.tar.xz"
+  sha256 "b4f3418154ef3f43d9420827294916aea1827021afc06e1644fc56951830a359"
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
 
   depends_on "gettext"
   depends_on "glib"
+  depends_on "icu4c"
   depends_on "json-glib"
   depends_on "libarchive" # need >= 3.0.0
-  depends_on "libsoup"
+  depends_on "libsoup@2"
 
   def install
-    args = %W[
+    args = std_meson_args + %W[
       --disable-dependency-tracking
       --disable-silent-rules
       --prefix=#{prefix}
       --localstatedir=#{var}
       --sysconfdir=#{etc}
     ]
-    system "./configure", *args
-    system "make", "install"
+
+    mkdir "build" do
+      system "meson", *args, ".."
+      system "ninja -v"
+      system "ninja", "install", "-v"
+    end
   end
 
   test do
